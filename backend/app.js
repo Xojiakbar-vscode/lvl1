9a775ac429247f24b6f60335d4c7fc2d86108d8b
muhammadrizo-0100 <mukhammadrizo2010.12.20@gmail.com>
2025-12-07 14:35:35 +0500

    log & reg

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const { sequelize } = require("./models");
-const adminRoute = require("./routes/adminRoute")
+const userRoute = require("./routes/userRoute")
 const setupSwagger = require("./swagger/swagger");
 
 dotenv.config();
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
-app.use("/api", adminRoute);
+app.use("/api", userRoute);
 
 setupSwagger(app);
 
@@ -23,6 +23,6 @@ sequelize
         console.log("Bazaga Ulandi ");
         app.listen(PORT, () => {
             console.log(`Server ishlayotgan manzil http://localhost:${PORT}`);
-        });
+    });
     })
     .catch((err) => console.error("Baza xatosi:", err));
--- backend/models/index.js
+++ backend/models/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const sequelize = require("../config/database");
 
-const Admin = require("./Admin.model")(sequelize, Sequelize);
+// const User = require("./userSchema")(sequelize, Sequelize);
 // const Customer = require("./customer.model")(sequelize, Sequelize);
 // const Car = require("./carSchema")(sequelize, Sequelize);
 
@@ -9,4 +9,4 @@ const Admin = require("./Admin.model")(sequelize, Sequelize);
 // User.associate(sequelize.models);
 // Customer.associate(sequelize.models);
 // Car.associate(sequelize.models);
-module.exports = { Admin, sequelize }
+module.exports = { sequelize}
